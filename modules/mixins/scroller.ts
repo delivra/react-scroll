6a225d2015d2c5d6185468e2c8363719261a443d
cluterbachdelivra <127438797+cluterbachdelivra@users.noreply.github.com>
2023-03-13 05:22:27 -0700

    Update scroller.ts
    removed referencePercent call
    edited deltaEnd to only use the offset and not the offsetHeight of the element so the element goes to topmost portion of the page if able.

--- modules/mixins/scroller.ts
+++ modules/mixins/scroller.ts
@@ -95,7 +95,7 @@ export class Scroller {
     const mapped = elements.map(element => {
       const offset = utils.scrollOffset(container, element, horizontal);
       const deltaStart = referenceOffset - offset;
-      const deltaEnd = horizontal ? referenceOffset - (offset + element.offsetWidth) : referenceOffset - (offset + element.offsetHeight);
+      const deltaEnd = referenceOffset - offset;
       const delta = Math.sign(deltaStart) !== Math.sign(deltaEnd) ? 0 : Math.min(Math.abs(deltaStart), Math.abs(deltaEnd));
       return { element, offset, delta };
     });
@@ -170,9 +170,9 @@ export class Scroller {
     }
 
     props.absolute = true;
-
-    let scrollOffset = utils.scrollOffset(containerElement, target, horizontal) + offset;
-    const containerPosition = utils.currentPosition(containerElement);
+    const pageTitleWrapperHeight = 105;
+    let scrollOffset = utils.scrollOffset(containerElement, target, horizontal) + offset - pageTitleWrapperHeight;
+    /* const containerPosition = utils.currentPosition(containerElement);
 
     //Override position to calculate based on the element position rather than scroll position
     const referencePercent = this.getReferencePoint(props, {
@@ -187,7 +187,7 @@ export class Scroller {
       //Correct the scrolloffset to put the viewport at the correct referencePoint
       scrollOffset -= referencePercent * containerPosition.height;
       scrollOffset += referencePercent * target.clientHeight;
-    }
+    } */
 
     /*
      * if animate is not provided just scroll into the view
